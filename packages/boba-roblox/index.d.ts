207b9758d4ea50db1433ca0d84c843724add7986
znotfireman <znotfireman@gmail.com>
2025-12-15 23:47:08 +0700

    feat: roblox codegen & license

--- packages/boba-roblox/index.d.ts
+++ packages/boba-roblox/index.d.ts
@@ -21,19 +21,4 @@
 
 */
 
-declare class Boba<T> {
-	static Any: Boba<any>;
-	static Unknown: Boba<unknown>;
-	static Never: Boba<never>;
-
-	static Boolean: Boba<boolean>;
-	static Buffer: Boba<buffer>;
-	static Function: Boba<(...args: any[]) => any>;
-	static Number: Boba<number>;
-	static String: Boba<string>;
-	static Table: Boba<object>;
-	static Thread: Boba<thread>;
-	static Vector: Boba<Vector>;
-}
-
-export = Boba;
+// Yeah i'm typing ts later
